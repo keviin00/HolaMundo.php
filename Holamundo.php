d824df547ed6231017dd29555aab0b44cb8d188b
keviin00 <90279937+keviin00@users.noreply.github.com>
2022-07-27 22:30:40 -0300

    Add files via upload

---
+++ Holamundo.php
@@ -0,0 +1,18 @@
+ <!doctype html>
+ <html>
+ <head>
+ <meta charset="utf-8">
+ <title>Documento sin titulo</title>
+ </head>
+ <body>
+
+<?php
+
+ 	print "Bienvenidos al curso de php <br>" ;  //br es salto de linea 
+ 	print "Hola Kevin <br>";
+    Print "VOS PODES ";
+
+
+?>
+ </body>
+ </html>
